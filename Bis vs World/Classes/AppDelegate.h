b2fc123ff70f2d43ce0576036fca31af3b8eb298
Weverton do Couto Timoteo <weverton@wolcan.us>
2014-02-12 00:14:40 -0200

    Initial Commit

---
+++ Bis vs World/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Bis vs World
+//
+//  Created by Weverton Couto Timoteo on 2/12/14.
+//  Copyright Weverton Couto Timoteo 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Bis vs World/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bis vs World
+//
+//  Created by Weverton Couto Timoteo on 2/12/14.
+//  Copyright Weverton Couto Timoteo 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
